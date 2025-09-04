ad48a66b19dda8d0e5f92a488cc2e88cc728f879
Carlos Marín <marin_moratalaz@outlook.com>
2025-09-04 17:49:40 +0200

    feat: bundle shellcommand dependency for pdftk

--- lib/pdftk/autoload.php
+++ lib/pdftk/autoload.php
@@ -3,6 +3,7 @@ spl_autoload_register(function ($class) {
     $prefixes = [
         'mikehaertl\\pdftk\\' => __DIR__ . '/pdftk/',
         'mikehaertl\\tmp\\'   => __DIR__ . '/tmp/',
+        'mikehaertl\\shellcommand\\' => __DIR__ . '/shellcommand/',
     ];
     foreach ($prefixes as $prefix => $baseDir) {
         $len = strlen($prefix);
