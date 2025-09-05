c4db9030c7057ec52f07969c44d0eea9fb0f165a
Carlos Marín <marin_moratalaz@outlook.com>
2025-09-05 10:52:05 +0200

    chore: add fontkit bundle

--- templates/parts/footer.php
+++ templates/parts/footer.php
@@ -118,6 +118,7 @@ use GarantiasOnline360VO\Svg;
         };
     </script>
     <script src="<?php echo esc_url(plugins_url('assets/js/pdf-lib.min.js', GARANTIAS360VO__FILE__)); ?>"></script>
+    <script src="<?php echo esc_url(plugins_url('assets/js/fontkit.umd.js', GARANTIAS360VO__FILE__)); ?>"></script>
     <script src="<?php echo esc_url(plugins_url('assets/js/nueva_garantia.min.js', GARANTIAS360VO__FILE__)); ?>" type="module" defer></script>
 
 <?php endif; ?>
