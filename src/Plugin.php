e0458e1fcf6fb7b1bde8c6c47ef15dfa65ffb2bb
Carlos Marín <marin_moratalaz@outlook.com>
2025-11-14 01:01:21 +0100

    Load translations before reset handlers

--- src/Plugin.php
+++ src/Plugin.php
@@ -50,7 +50,7 @@ class Plugin
         add_action('init', [ActivitySubscribers::class, 'init'], 5);
         add_action('login_init', [ActivitySubscribers::class, 'init']);
         add_action('init', [PrivateDocsManager::class, 'ensure_directory']);
-        add_action('init', [__CLASS__, 'load_textdomain']);
+        add_action('plugins_loaded', [__CLASS__, 'load_textdomain']);
         add_action('rest_api_init', [\GarantiasOnline360VO\Rest\GuaranteeRestController::class, 'register_routes']);
         \GarantiasOnline360VO\Rest\GuaranteeRestController::register_cache_hooks();
 
