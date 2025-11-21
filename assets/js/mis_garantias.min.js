dfaf732ea6a36a121ca3a219516c8ab9d00cac68
Carlos Marín <marin_moratalaz@outlook.com>
2025-11-21 03:50:06 +0100

    Define caret icon used by manage button

--- assets/js/mis_garantias.min.js
+++ assets/js/mis_garantias.min.js
@@ -339,6 +339,7 @@ const ADD_DOC_KEY = "add-document";
                         '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m438-338 226-226-57-57-169 169-84-84-57 57 141 141Zm42 258q-139-35-229.5-159.5T160-516v-244l320-120 320 120v244q0 152-90.5 276.5T480-80Zm0-84q104-33 172-132t68-220v-189l-240-90-240 90v189q0 121 68 220t172 132Zm0-316Z"/></svg>';
                 const managementArrowIcon =
                         '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 -960 960 960" fill="currentColor"><path d="M360-200 320-240 520-440 320-640l40-40 240 240-240 240Z"/></svg>';
+                const caretRightIcon = managementArrowIcon;
                 const ADMIN_SUMMARY_ACTIONS = [
                         {
                                 key: "draft",
